26ed54621f7063a41819a9fe13a9329cd8585ff6
Andrea Sonny <andreasonny83@gmail.com>
2015-10-01 21:48:30 +0100

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,16 +5,24 @@ Copyright (c) 2015 LanguageQUIZ. All rights reserved.
 (function(document) {
 	'use strict';
 
+	var lqApp = document.querySelector( '#lqApp' );
+
 	window.addEventListener( 'WebComponentsReady', function() {
 		// imports are loaded and elements have been registered
-		window.toast            = document.querySelector( '#toast' );
-		window.lqMain           = document.querySelector( '#lqMain' );
-		window.lqLogin          = document.querySelector( '#lqLogin' );
-		window.lqHome           = document.querySelector( '#lqHome' );
-		window.lqUserProfile    = document.querySelector( '#lqUserProfile' );
-		window.lqUserCollection = document.querySelector( '#lqUserCollection' );
-		window.lqCollectionEdit = document.querySelector( '#lqCollectionEdit' );
+		// window.toast            = document.querySelector( '#toast' );
+		// window.lqMain           = document.querySelector( '#lqMain' );
+		// window.lqLogin          = document.querySelector( '#lqLogin' );
+		// window.lqHome           = document.querySelector( '#lqHome' );
+		// window.lqUserProfile    = document.querySelector( '#lqUserProfile' );
+		// window.lqUserCollection = document.querySelector( '#lqUserCollection' );
+		// window.lqCollectionEdit = document.querySelector( '#lqCollectionEdit' );
 
 		console.log( 'LanguageQUIZ is ready!' );
 	});
+
+	// Scroll page to top and expand header
+	lqApp.scrollPageToTop = function() {
+		document.getElementById( 'mainContainer' ).scrollTop = 0;
+	};
+
 })(document);
