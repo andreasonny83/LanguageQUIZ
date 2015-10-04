22a8862332d8ef568c353263baff84ba0729a1f0
Andrea Sonny <andreasonny83@gmail.com>
2015-10-04 21:54:45 +0100

    Favorites v1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,15 +8,6 @@ Copyright (c) 2015 LanguageQUIZ. All rights reserved.
 	var lqApp = document.querySelector( '#lqApp' );
 
 	window.addEventListener( 'WebComponentsReady', function() {
-		// imports are loaded and elements have been registered
-		// window.toast            = document.querySelector( '#toast' );
-		// window.lqMain           = document.querySelector( '#lqMain' );
-		// window.lqLogin          = document.querySelector( '#lqLogin' );
-		// window.lqHome           = document.querySelector( '#lqHome' );
-		// window.lqUserProfile    = document.querySelector( '#lqUserProfile' );
-		// window.lqUserCollection = document.querySelector( '#lqUserCollection' );
-		// window.lqCollectionEdit = document.querySelector( '#lqCollectionEdit' );
-
 		console.log( 'LanguageQUIZ is ready!' );
 	});
 
