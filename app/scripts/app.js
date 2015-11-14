63ac93b346e920c4f5e7ba290e8cbceeda0f8c28
Andrea Sonny <andreasonny83@gmail.com>
2015-11-14 12:29:33 +0000

    Code refactoring

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,18 @@
 /*
 Copyright (c) 2015 LanguageQUIZ. All rights reserved.
 */
+( function( document ) {
+  'use strict';
 
-(function(document) {
-	'use strict';
+  var lqApp = document.querySelector( '#lqApp' );
 
-	var lqApp = document.querySelector( '#lqApp' );
+  window.addEventListener( 'WebComponentsReady', function() {
+    console.log( 'LanguageQUIZ is ready!' );
+  });
 
-	window.addEventListener( 'WebComponentsReady', function() {
-		console.log( 'LanguageQUIZ is ready!' );
-	});
-
-	// Scroll page to top and expand header
-	lqApp.scrollPageToTop = function() {
-		document.getElementById( 'mainContainer' ).scrollTop = 0;
-	};
+  // Scroll page to top and expand header
+  lqApp.scrollPageToTop = function() {
+    document.getElementById( 'mainContainer' ).scrollTop = 0;
+  };
 
 })(document);
